e58032ca4a32d7a48f34b604ac40a09d47e2d284
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2021-03-31 09:16:36 +0200

    sum multiplication

--- 03-sum-multiplication/js/script.js
+++ 03-sum-multiplication/js/script.js
@@ -1,3 +1,17 @@
 let table = [1, 2, 3, 4, 5, 6];
 
-console.log(table);
+// nie nazywamy parametrow tak jak zmiennych
+const sumAndMultiplication = (tab) => {
+    let sum = 0;
+    let multi = 1;
+
+    for (const el of tab) {
+        console.log(el);
+        sum += el;
+        multi *= el;
+    }
+    //console.log(tab);
+    console.log([sum, multi]);
+}
+
+sumAndMultiplication(table);
