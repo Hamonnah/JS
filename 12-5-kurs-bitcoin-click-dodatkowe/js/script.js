a32ae0235c505680da72c30940721ba41af611c6
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2021-08-03 05:28:04 +0200

    funcion

--- 12-5-kurs-bitcoin-click-dodatkowe/js/script.js
+++ 12-5-kurs-bitcoin-click-dodatkowe/js/script.js
@@ -7,87 +7,51 @@ $(document).ready(function () {
 
     let currentExangeValue = 0;
 
-    $.getJSON('https://blockchain.info/pl/ticker')
-            .done(function (data) {
-
-                if (currentExangeValue < data.PLN.buy) {
-                    $('#indicator').removeAttr('class').addClass('fas fa-arrow-up ');
-                    $('#indicator-buy').removeAttr('class').addClass('fas fa-arrow-up');
-                } else if (currentExangeValue > data.PLN.buy) {
-                    $('#indicator').removeAttr('class').addClass('fas fa-arrow-down');
-                    $('#indicator-buy').removeAttr('class').addClass('fas fa-arrow-down');
-                } else {
-                    $('#indicator').removeAttr('class').addClass('fas fa-minus');
-                    $('#indicator-buy').removeAttr('class').addClass('fas fa-minus');
-                }
-
-                let pTitle = $('<h1></h1>').text(`Aktualny kurs Bitcoin do PLN`);
-                let titleBuy = $('<h2></h2>').text(`Kupno : `);
-                let pBuy = $('<p></p>').text(`${data.PLN.buy} ${data.PLN.symbol}`);
-                let titleSell = $('<h2></h2>').text(`Sprzedaz : `);
-                let pSell = $('<p></p>').text(`${data.PLN.sell} ${data.PLN.symbol}`);
-                let jqBuyExange = $('.buy-exange');
-                let jqsellExange = $('.sell-exange');
-                let jqBox = $('#box')
-
-                jqBuyExange.empty();
-                jqsellExange.empty();
-                jqBox.empty();
-                jqBox.append(pTitle);
-                jqBuyExange.append(titleBuy);
-                jqBuyExange.append(pBuy);
-                jqsellExange.append(titleSell);
-                jqsellExange.append(pSell);
-
-                //$('h1').css('background-color', 'red');
-                currentExangeValue = data.PLN.buy;
-
-            })
-            .fail(function (error) {
-                console.error(error);
-            });
-
-    $('#get-data').click(function () {
+    const getData = () => {
         $.getJSON('https://blockchain.info/pl/ticker')
-            .done(function (data) {
+        .done(function (data) {
 
-                if (currentExangeValue < data.PLN.buy) {
-                    $('#indicator').removeAttr('class').addClass('fas fa-arrow-up ');
-                    $('#indicator-buy').removeAttr('class').addClass('fas fa-arrow-up');
-                } else if (currentExangeValue > data.PLN.buy) {
-                    $('#indicator').removeAttr('class').addClass('fas fa-arrow-down');
-                    $('#indicator-buy').removeAttr('class').addClass('fas fa-arrow-down');
-                } else {
-                    $('#indicator').removeAttr('class').addClass('fas fa-minus');
-                    $('#indicator-buy').removeAttr('class').addClass('fas fa-minus');
-                }
+            if (currentExangeValue < data.PLN.buy) {
+                $('#indicator').removeAttr('class').addClass('fas fa-arrow-up ');
+                $('#indicator-buy').removeAttr('class').addClass('fas fa-arrow-up');
+            } else if (currentExangeValue > data.PLN.buy) {
+                $('#indicator').removeAttr('class').addClass('fas fa-arrow-down');
+                $('#indicator-buy').removeAttr('class').addClass('fas fa-arrow-down');
+            } else {
+                $('#indicator').removeAttr('class').addClass('fas fa-minus');
+                $('#indicator-buy').removeAttr('class').addClass('fas fa-minus');
+            }
 
-                let pTitle = $('<h1></h1>').text(`Aktualny kurs Bitcoin do PLN`);
-                let titleBuy = $('<h2></h2>').text(`Kupno : `);
-                let pBuy = $('<p></p>').text(`${data.PLN.buy} ${data.PLN.symbol}`);
-                let titleSell = $('<h2></h2>').text(`Sprzedaz : `);
-                let pSell = $('<p></p>').text(`${data.PLN.sell} ${data.PLN.symbol}`);
-                let jqBuyExange = $('.buy-exange');
-                let jqsellExange = $('.sell-exange');
-                let jqBox = $('#box')
+            let pTitle = $('<h1></h1>').text(`Aktualny kurs Bitcoin do PLN`);
+            let titleBuy = $('<h2></h2>').text(`Kupno : `);
+            let pBuy = $('<p></p>').text(`${data.PLN.buy} ${data.PLN.symbol}`);
+            let titleSell = $('<h2></h2>').text(`Sprzedaz : `);
+            let pSell = $('<p></p>').text(`${data.PLN.sell} ${data.PLN.symbol}`);
+            let jqBuyExange = $('.buy-exange');
+            let jqsellExange = $('.sell-exange');
+            let jqBox = $('#box')
 
-                jqBuyExange.empty();
-                jqsellExange.empty();
-                jqBox.empty();
-                jqBox.append(pTitle);
-                jqBuyExange.append(titleBuy);
-                jqBuyExange.append(pBuy);
-                jqsellExange.append(titleSell);
-                jqsellExange.append(pSell);
+            jqBuyExange.empty();
+            jqsellExange.empty();
+            jqBox.empty();
+            jqBox.append(pTitle);
+            jqBuyExange.append(titleBuy);
+            jqBuyExange.append(pBuy);
+            jqsellExange.append(titleSell);
+            jqsellExange.append(pSell);
 
-                //$('h1').css('background-color', 'red');
-                currentExangeValue = data.PLN.buy;
+            currentExangeValue = data.PLN.buy;
 
-            })
-            .fail(function (error) {
-                console.error(error);
-            });
-
+        })
+        .fail(function (error) {
+            console.error(error);
+        });
+    }
+    
+    getData();
+    
+    $('#get-data').click(function () {
+        getData();
     });
 
 });
